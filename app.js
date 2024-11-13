1f32c2352751f668be7073ca412a3204848b6a15
omware12345 <omwareinfosystem@gmail.com>
2024-11-12 19:35:06 -0800

    envornment variable

--- app.js
+++ app.js
@@ -11,13 +11,14 @@ const cors = require('cors');
 const thali = require('./router/thali');
 const hotal = require('./router/hotal');
 const path = require('path');
+const dotenv = require('dotenv');
 app.use(cors());
+dotenv.config();
 
 // ----------------DATABASE CONNECTION---------------------
-
-mongo.connect('mongodb+srv://amitkumarsingh1482:amit1234@document.dkfjdwr.mongodb.net/document').
-    then(() => console.log("DB Connected!!")).
-    catch((err) => console.log({ "Error": err }));
+mongo.connect(`${process.env.DB}`).
+  then(() => console.log("DB Connected!!")).
+  catch((err) => console.log({ "Error": err }));
 
 app.use(bodyParser.json());
 
@@ -26,11 +27,11 @@ app.use('/api/v2', admin);
 app.use('/api/v3', thali);
 app.use('/api/v4', hotal);
 
-app.use(express.static(path.join(__dirname,'../','METRODELICIOUS-FRONTEND/public')));
+app.use(express.static(path.join(__dirname, '../', 'METRODELICIOUS-FRONTEND/public')));
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname,'../','METRODELICIOUS-FRONTEND/public/index.html'));
+  res.sendFile(path.join(__dirname, '../', 'METRODELICIOUS-FRONTEND/public/index.html'));
 });
 
 // ----------------SERVER CONNECTION---------------------
 
-app.listen(port, () => console.log("Server Created !!"));
+app.listen(process.env.PORT || port, () => console.log("Server Created !!"));
